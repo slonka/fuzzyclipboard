9ba884af1b88307eb7cad1f0de3b7700867033ec
Krzysztof Słonka <krzysztof.slonka@schibsted.pl>
2016-01-20 16:45:20 +0100

    Format code and add minimize to tray

--- app/js/index.js
+++ app/js/index.js
@@ -5,14 +5,14 @@ var gui = require('nw.gui');
 var win = gui.Window.get();
 
 var option = {
-  key : "Ctrl+Shift+V",
-  active : function() {
-    console.log("Global desktop keyboard shortcut: " + this.key + " active.");
-  },
-  failed : function(msg) {
-    // :(, fail to register the |key| or couldn't parse the |key|.
-    console.log(msg);
-  }
+    key: "Ctrl+Shift+V",
+    active: function() {
+        console.log("Global desktop keyboard shortcut: " + this.key + " active.");
+    },
+    failed: function(msg) {
+        // :(, fail to register the |key| or couldn't parse the |key|.
+        console.log(msg);
+    }
 };
 
 var util = require('util');
@@ -24,7 +24,25 @@ var shortcut = new gui.Shortcut(option);
 // Register global desktop shortcut, which can work without focus.
 gui.App.registerGlobalHotKey(shortcut);
 
-win.on('close', function () {
-  // Unregister the global desktop shortcut.
-  gui.App.unregisterGlobalHotKey(shortcut);
+win.on('close', function() {
+    // Unregister the global desktop shortcut.
+    gui.App.unregisterGlobalHotKey(shortcut);
+});
+
+// Get the minimize event
+win.on('minimize', function() {
+    // Hide window
+    this.hide();
+
+    // Show tray
+    tray = new gui.Tray({
+        icon: 'icon.png'
+    });
+
+    // Show window and remove tray when clicked
+    tray.on('click', function() {
+        win.show();
+        this.remove();
+        tray = null;
+    });
 });
