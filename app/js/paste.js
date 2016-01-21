08b609333c568c0c3c4e776784ada5c2de72e7f2
Krzysztof Słonka <krzysztof.slonka@schibsted.pl>
2016-01-21 13:56:05 +0100

    Fix pasted command appearing twice in history. Add onclick paste functionality

--- app/js/paste.js
+++ app/js/paste.js
@@ -5,6 +5,8 @@ var clipboard = require('./clipboard.js');
 var script = 'osascript -e \'tell application "System Events" to keystroke tab using {command down} \n delay 0.5 \n tell application "System Events" to keystroke "v" using command down \n delay 0.5 \'';
 
 module.exports = function(text) {
-  clipboard.set(text, 'text');
+  if(clipboard.get('text') !== text) {
+    clipboard.set(text, 'text');
+  }
   child_process.execSync(script);
 }
